b01b50e24a3de661f0e255d80a2ed1fca5808d51
Antoine James Tournepiche <26577763+AntoineJT@users.noreply.github.com>
2019-01-28 11:49:34 +0100

    Cleaning code
    The source code has been cleaned up!

--- main.c
+++ main.c
@@ -1,101 +1,115 @@
 /*
     main.c
-    Unique file of the ASCII TicTacToe
+    Main file of the ASCII TicTacToe
     Written by Antoine James Tournepiche
-    10/21/2018 - 11/03/2018 13:19
+    10/21/2018 - 01/28/2019 11:00
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "boolean.h"
 
-// Boolean
-typedef unsigned char boolean;
-#define BFALSE 0
-#define BTRUE 1
-
-typedef unsigned int player; // Possible micro-optimization : check if unsigned char is faster / as fast as unsigned int to reduce RAM consumption (not very useful because this is just a tic tac toe in ASCII and the optimization will not be visible at all)
-#define PLAYER_NULL 2 // Used to indicates when no one get a frame or when it has no winner for now
-#define PLAYER_CROSS 0
-#define PLAYER_CIRCLE 1
-#define PLAYER_UNDEFINED 3 // Used to indicates when it has equality
+typedef enum {
+    PLAYER_CROSS=0,
+    PLAYER_CIRCLE=1,
+    PLAYER_NULL,      // Used to indicates when no one get a frame or when it has no winner for now
+    PLAYER_UNDEFINED  // Used to indicates when it has equality
+} player;
 
 player plycells[9];
+char grid[9];
 
-player getWinner(){
+static player getWinner(void){
     if (plycells[4]!=PLAYER_NULL&&(
         (plycells[3]==plycells[4]&&plycells[4]==plycells[5])|| // HLine 2
         (plycells[1]==plycells[4]&&plycells[4]==plycells[7])|| // VLine 2
         (plycells[0]==plycells[4]&&plycells[4]==plycells[8])|| // DLine '\'
         (plycells[2]==plycells[4]&&plycells[4]==plycells[6]) // DLine /
-    )){
+    ))
         return plycells[4];
-    } else if (plycells[0]!=PLAYER_NULL&&(
+    else if (plycells[0]!=PLAYER_NULL&&(
         (plycells[0]==plycells[1]&&plycells[1]==plycells[2])|| // HLine 1
         (plycells[0]==plycells[3]&&plycells[3]==plycells[6]) // VLine 1
-    )){
+    ))
         return plycells[0];
-    } else if (plycells[8]!=PLAYER_NULL&&(
+    else if (plycells[8]!=PLAYER_NULL&&(
         (plycells[6]==plycells[7]&&plycells[7]==plycells[8])|| // HLine 3
         (plycells[2]==plycells[5]&&plycells[5]==plycells[8]) // VLine 3
-    )){
+    ))
         return plycells[8];
-    } else {
-        for (int i=0;i<9;i++)
+    else {
+        for (register unsigned int i=0;i<9;i++)
             if (plycells[i]==PLAYER_NULL)
                 return PLAYER_NULL; // No winner for now
         return PLAYER_UNDEFINED; // Equality
     }
 }
 
-char* getPlayerName(const player pl){
-    if (pl==PLAYER_CROSS)
-        return "Cross";
-    else if (pl==PLAYER_CIRCLE)
-        return "Circle";
-    else
-        return "ERROR";
+static char* getPlayerName(const player pl){
+    switch(pl){
+        case PLAYER_CROSS:
+            return "Cross";
+            break;
+        case PLAYER_CIRCLE:
+            return "Circle";
+            break;
+        default:
+            return "ERROR";
+            break;
+    }
 }
 
-boolean playCell(const unsigned int cell,const player pl){
-    if (plycells[cell]==PLAYER_NULL){
-        plycells[cell]=pl;
-        return BTRUE;
-    } else
-        return BFALSE;
+static boolean playCell(const unsigned int cell,const player pl){
+    if(plycells[cell]!=PLAYER_NULL)
+        return false;
+    plycells[cell]=pl;
+    return true;
 }
 
-char grid[9];
-// PRINTGRID was a void function before, but I think it's better optimize to change it to a macro
-#define PRINTGRID \
-    for (int i=0;i<9;i++) \
-        if (plycells[i]==PLAYER_NULL) \
-            grid[i]=' '; \
-        else if (plycells[i]==PLAYER_CROSS) \
-            grid[i]='X'; \
-        else if (plycells[i]==PLAYER_CIRCLE) \
-            grid[i]='O'; \
+static void printGrid(void){
+    for(unsigned int i=0;i<9;i++)
+        if (plycells[i]==PLAYER_NULL)
+            grid[i]=' ';
+        else if (plycells[i]==PLAYER_CROSS)
+            grid[i]='X';
+        else if (plycells[i]==PLAYER_CIRCLE)
+            grid[i]='O';
     printf(" _________________ \n|**0**|**1**|**2**|\n|  %c  |  %c  |  %c  |\n|_____|_____|_____|\n|**3**|**4**|**5**|\n|  %c  |  %c  |  %c  |\n|_____|_____|_____|\n|**6**|**7**|**8**|\n|  %c  |  %c  |  %c  |\n|_____|_____|_____|\n",grid[0],grid[1],grid[2],grid[3],grid[4],grid[5],grid[6],grid[7],grid[8]);
+}
 
-#define INIT \
-	for (int i=0;i<9;i++) \
+static void input_cell(const player pl,unsigned int *cell){
+    boolean valid;
+    do {
+        fputs("Select the cell you want to play : ",stdout);
+        valid=scanf("%u",cell)&&*cell<10; // if the input is invalid or out of range, it will ask again to user
+        if(!valid){
+            while(getchar()!='\n');
+            puts("Invalid cell number!");
+        } else {
+            valid=playCell(*cell,pl);
+            if(!valid)
+                puts("This Cell is already owned!");
+        }
+    } while(!valid);
+}
+
+static boolean input_bool(const char *str){
+    boolean valid;
+    char choice;
+    do {
+        printf("%s [Y/N]\n",str);
+        while(getchar()!='\n');
+        valid=scanf("%[YyNn]",&choice);
+        if(!valid)
+            puts("Invalid choice!");
+    } while(!valid);
+    return (choice=='Y'||choice=='y') ? true : false;
+}
+
+static inline void init(void){
+    for (int i=0;i<9;i++)
         plycells[i]=PLAYER_NULL;
-
-#define RESET \
-    printf("Do you want to restart a game ? (type 1 to accept)\n"); \
-    int i_=scanf("%d",&i); \
-    if (i==1&&i_) \
-        INIT \
-    else \
-        stay=BFALSE; \
-    \
-    i=PLAYER_NULL;
-
-/* This macro allows to do secure scanf */
-#define SCANF(_FORMAT_,_POINTER_) \
-	if (!scanf(_FORMAT_,_POINTER_)){ \
-		fprintf(stderr,"You have entered an invalid value and the program has crashed!"); \
-		exit(EXIT_FAILURE); \
-	}
+}
 
 #ifdef _WIN32
     #include <windows.h>
@@ -107,47 +121,32 @@ char grid[9];
 #endif
 int main(void)
 {
-    CMD_TITLE("ASCII Tic Tac Toe - made by Antoine James Tournepiche");
-    printf("ASCII Tic Tac Toe\nmade by Antoine James Tournepiche\n10/21/2018 - 11/03/2018 13:19\n\n");
-    INIT // Here I use a macro just because it's clearer
-    boolean stay=BTRUE;
+    CMD_TITLE("ASCII Tic Tac Toe - made by Antoine James Tournepiche")
+    puts("ASCII Tic Tac Toe\nmade by Antoine James Tournepiche\n10/21/2018 - 11/03/2018 13:19\n");
+    init();
     player pl=PLAYER_CROSS; // First player to play
-    player i=PLAYER_NULL; // Winner, or Cell number (2 usages)
-    while(stay){
-        PRINTGRID
+    unsigned int i=PLAYER_NULL; // Winner, or Cell number (2 usages)
+    while(1){ // main loop
+        printGrid();
         if (i==PLAYER_NULL){
             printf("It's the turn of the %s player.\n",getPlayerName(pl));
-            do {
-                printf("Select the cell you want to play : ");
-                SCANF("%d",&i)
-                if (i>8)
-                    printf("Invalid Cell Number!\n");
-                else if (!playCell(i,pl)){
-                    printf("This Cell is already owned!\n");
-                    i=9;
-                }
-            } while (i>8);
+            input_cell(pl,&i);
             i=getWinner();
-        } else if (i==PLAYER_UNDEFINED){
-            printf("Equality !\n");
-            RESET
         } else {
-            pl=!pl; // Here is a fix to print the correct winner : here we don't want to print the next player to play, but the last player which has played : the winner
-            printf("The player %s has WIN!\n",getPlayerName(pl));
-            RESET
+            if (i==PLAYER_UNDEFINED)
+                puts("Equality !");
+            else {
+                pl=!pl; // Here is a fix to print the correct winner : here we don't want to print the next player to play, but the last player which has played : the winner
+                printf("The player %s has WIN!\n",getPlayerName(pl));
+            }
+            // reset code
+            if(input_bool("Do you want to restart a game ?"))
+                init();
+            else
+                exit(EXIT_SUCCESS);
+            i=PLAYER_NULL;
         }
         pl=!pl; // Switch from CIRCLE to CROSS, or from CROSS to CIRCLE
     }
     return EXIT_SUCCESS;
 }
-
-#undef BTRUE
-#undef BFALSE
-#undef PLAYER_NULL
-#undef PLAYER_CROSS
-#undef PLAYER_CIRCLE
-#undef PLAYER_UNDEFINED
-#undef PRINTGRID
-#undef INIT
-#undef RESET
-#undef SCANF
