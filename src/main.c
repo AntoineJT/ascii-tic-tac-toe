22e33c6a3c253e95114247579fb605cf974c529a
AntoineJT <antoinejt.serveur@gmail.com>
2019-03-28 16:58:37 +0100

    Improve code quality and readability

--- src/main.c
+++ src/main.c
@@ -29,37 +29,32 @@ static player getWinner(void){
         (plycells[1]==plycells[4]&&plycells[4]==plycells[7])|| // VLine 2
         (plycells[0]==plycells[4]&&plycells[4]==plycells[8])|| // DLine '\'
         (plycells[2]==plycells[4]&&plycells[4]==plycells[6]) // DLine /
-    ))
+    )){
         return plycells[4];
-    else if (plycells[0]!=PLAYER_NULL&&(
+    } else if (plycells[0]!=PLAYER_NULL&&(
         (plycells[0]==plycells[1]&&plycells[1]==plycells[2])|| // HLine 1
         (plycells[0]==plycells[3]&&plycells[3]==plycells[6]) // VLine 1
-    ))
+    )){
         return plycells[0];
-    else if (plycells[8]!=PLAYER_NULL&&(
+    } else if (plycells[8]!=PLAYER_NULL&&(
         (plycells[6]==plycells[7]&&plycells[7]==plycells[8])|| // HLine 3
         (plycells[2]==plycells[5]&&plycells[5]==plycells[8]) // VLine 3
-    ))
+    )){
         return plycells[8];
-    else {
-        for (register unsigned int i=0;i<9;i++)
+    } else {
+        for (register unsigned int i=0;i<9;i++){
             if (plycells[i]==PLAYER_NULL)
                 return PLAYER_NULL; // No winner for now
+        }
         return PLAYER_UNDEFINED; // Equality
     }
 }
 
 static char* getPlayerName(const player pl){
     switch(pl){
-        case PLAYER_CROSS:
-            return "Cross";
-            break;
-        case PLAYER_CIRCLE:
-            return "Circle";
-            break;
-        default:
-            return "ERROR";
-            break;
+        case PLAYER_CROSS: return "Cross";
+        case PLAYER_CIRCLE: return "Circle";
+        default: return "ERROR";
     }
 }
 
@@ -71,26 +66,36 @@ static boolean playCell(const unsigned int cell,const player pl){
 }
 
 static void printGrid(void){
-    for(unsigned int i=0;i<9;i++)
-        if (plycells[i]==PLAYER_NULL)
-            grid[i]=' ';
-        else if (plycells[i]==PLAYER_CROSS)
-            grid[i]='X';
-        else if (plycells[i]==PLAYER_CIRCLE)
-            grid[i]='O';
-    printf(" _________________ \n|**0**|**1**|**2**|\n|  %c  |  %c  |  %c  |\n|_____|_____|_____|\n|**3**|**4**|**5**|\n|  %c  |  %c  |  %c  |\n|_____|_____|_____|\n|**6**|**7**|**8**|\n|  %c  |  %c  |  %c  |\n|_____|_____|_____|\n",grid[0],grid[1],grid[2],grid[3],grid[4],grid[5],grid[6],grid[7],grid[8]);
+    for(unsigned int i=0;i<9;i++){
+        switch(plycells[i]){
+            case PLAYER_NULL: grid[i]=' '; break;
+            case PLAYER_CROSS: grid[i]='X'; break;
+            case PLAYER_CIRCLE: grid[i]='O'; break;
+            default: return;
+        }
+    }
+    printf(" _________________ " "\n"
+           "|**0**|**1**|**2**|" "\n"
+           "|  %c  |  %c  |  %c  |" "\n"
+           "|_____|_____|_____|" "\n"
+           "|**3**|**4**|**5**|" "\n"
+           "|  %c  |  %c  |  %c  |" "\n"
+           "|_____|_____|_____|" "\n"
+           "|**6**|**7**|**8**|" "\n"
+           "|  %c  |  %c  |  %c  |" "\n"
+           "|_____|_____|_____|" "\n",grid[0],grid[1],grid[2],grid[3],grid[4],grid[5],grid[6],grid[7],grid[8]);
 }
 
 static void input_cell(const player pl,unsigned int *cell){
     boolean valid;
     do {
         fputs("Select the cell you want to play : ",stdout);
-        valid=scanf("%u",cell)&&*cell<10; // if the input is invalid or out of range, it will ask again to user
+        valid = scanf("%u",cell) && *cell<10; // if the input is invalid or out of range, it will ask again to user
         if(!valid){
             while(getchar()!='\n');
             puts("Invalid cell number!");
         } else {
-            valid=playCell(*cell,pl);
+            valid = playCell(*cell,pl);
             if(!valid)
                 puts("This Cell is already owned!");
         }
@@ -107,7 +112,7 @@ static boolean input_bool(const char *str){
         if(!valid)
             puts("Invalid choice!");
     } while(!valid);
-    return (choice=='Y'||choice=='y') ? true : false;
+    return (choice=='Y'||choice=='y');
 }
 
 static inline void init(void){
@@ -128,19 +133,19 @@ int main(void)
     CMD_TITLE("ASCII Tic Tac Toe - made by Antoine James Tournepiche")
     puts("ASCII Tic Tac Toe\nmade by Antoine James Tournepiche\n10/21/2018 - 11/03/2018 13:19\n");
     init();
-    player pl=PLAYER_CROSS; // First player to play
-    unsigned int i=PLAYER_NULL; // Winner, or Cell number (2 usages)
+    player pl = PLAYER_CROSS; // First player to play
+    unsigned int i = PLAYER_NULL; // Winner, or Cell number (2 usages)
     while(1){ // main loop
         printGrid();
-        if (i==PLAYER_NULL){
+        if (i == PLAYER_NULL){
             printf("It's the turn of the %s player.\n",getPlayerName(pl));
             input_cell(pl,&i);
-            i=getWinner();
+            i = getWinner();
         } else {
-            if (i==PLAYER_UNDEFINED)
+            if (i == PLAYER_UNDEFINED)
                 puts("Equality !");
             else {
-                pl=!pl; // Here is a fix to print the correct winner : here we don't want to print the next player to play, but the last player which has played : the winner
+                pl = !pl; // Here is a fix to print the correct winner : here we don't want to print the next player to play, but the last player which has played : the winner
                 printf("The player %s has WIN!\n",getPlayerName(pl));
             }
             // reset code
@@ -148,9 +153,9 @@ int main(void)
                 init();
             else
                 exit(EXIT_SUCCESS);
-            i=PLAYER_NULL;
+            i = PLAYER_NULL;
         }
-        pl=!pl; // Switch from CIRCLE to CROSS, or from CROSS to CIRCLE
+        pl = !pl; // Switch from CIRCLE to CROSS, or from CROSS to CIRCLE
     }
     return EXIT_SUCCESS;
 }
