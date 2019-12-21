61b155c18db8223d139f36a2775274fa171b8c5b
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-12-21 12:32:17 +0100

    :recycle: Refactor rename stuff, create a function to free ttt_board

--- src/ttt/board.c
+++ src/ttt/board.c
@@ -2,7 +2,7 @@
 	Name : ttt/board.c
 	Author : Antoine James Tournepiche
 	Creation Date : December 20th 2019
-	Last update : December 20th 2019
+	Last update : December 21st 2019
 	Project : ASCII Tic Tac Toe
 	Project sources : https://github.com/AntoineJT/ascii-tic-tac-toe
 
@@ -11,11 +11,19 @@
 
 #include <stdlib.h>
 #include "board.h"
+#include "../boolean.h"
 
-ttt_board* create_ttt_board(void) {
+ttt_board* ttt_create_board(void)
+{
 	ttt_board* board_ptr = malloc(sizeof(ttt_board));
-	if (board_ptr == NULL) {
+	if (board_ptr == NULL) 
+	{
 		return NULL;
 	}
 	return board_ptr;
+}
+
+void ttt_destroy_board(ttt_board* board)
+{
+	free(board);
 }
--- src/ttt/board.h
+++ src/ttt/board.h
@@ -2,7 +2,7 @@
 	Name : ttt/board.h
 	Author : Antoine James Tournepiche
 	Creation Date : December 20th 2019
-	Last update : December 20th 2019
+	Last update : December 21st 2019
 	Project : ASCII Tic Tac Toe
 	Project sources : https://github.com/AntoineJT/ascii-tic-tac-toe
 
@@ -18,5 +18,6 @@
 		char grid[9];
 	} ttt_board;
 
-	ttt_board* create_ttt_board(void);
+	ttt_board* ttt_create_board(void);
+	void ttt_destroy_board(ttt_board* board);
 #endif
