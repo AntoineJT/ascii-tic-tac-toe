e2f8d2c0fce22ed38381836c661427827835e649
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-02-24 22:17:45 +0100

    :white_check_mark: Add some test cases

--- tests/ttt_player_test.h
+++ tests/ttt_player_test.h
@@ -7,3 +7,11 @@
     check(ttt_get_player_name(PLAYER_NULL) == "ERROR");
     check(ttt_get_player_name(PLAYER_UNDEFINED) == "ERROR");
 }
+
+it("should opponent be correct")
+{
+    check(ttt_get_opponent(PLAYER_CROSS) == PLAYER_CIRCLE);
+    check(ttt_get_opponent(PLAYER_CIRCLE) == PLAYER_CROSS);
+    check(ttt_get_opponent(PLAYER_UNDEFINED) == PLAYER_NULL);
+    check(ttt_get_opponent(PLAYER_NULL) == PLAYER_NULL);
+}
